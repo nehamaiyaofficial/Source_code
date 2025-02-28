2c56ca8423dc5974a1a67ea881c786d9c14f9b1c
nehamaiyaoffical <neha.maiya@s.amity.edu>
2025-02-28 12:12:22 +0530

    6th

--- program.py
+++ program.py
@@ -1,14 +1,4 @@
-balance = float(input("Enter your bank account balance: "))
-withdraw_amount = float(input("Enter the amount you want to withdraw: "))
-if balance >= withdraw_amount:
-    if withdraw_amount >= 1000:
-        print("Transaction Successful! ₹", withdraw_amount, "withdrawn.")
-        balance -= withdraw_amount  
-        print("Remaining Balance: ₹", balance)
-    else:
-        print("Error! Minimum withdrawal amount is ₹1000.")
-else:
-    print("Insufficient balance! Transaction failed.")
-
+for num in range(1, 101, 2): 
+    print(num)
 
 
